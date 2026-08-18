8a3985fe33826242af02e1246958b9c75f536646
SteveMaranga01 <stevemaranga09@gmail.com>
2026-08-18 18:24:23 +0300

    testimonials added

---
+++ components/sections/testimonials.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Testimonials() {
+  return(
+    <section className="ybit-container relative z-10 flex min-h-[100svh] items-end pb-12 pt-32 md:pb-20"></section>
+  );
+}
